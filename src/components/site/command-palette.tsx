5c150d6b63210a73f2bf52304836c0e4f3809a29
Rami-Ebro <ahr17h@gmail.com>
2026-06-29 15:20:43 +0300

    Remove merge conflict markers from command-palette.tsx

--- src/components/site/command-palette.tsx
+++ src/components/site/command-palette.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { useEffect } from "react";
@@ -238,4 +237,3 @@ export function CommandPalette({
     </Dialog>
   );
 }
->>>>>>> 0d2d90e9f17bc15315fda8af6702b754efb3467d
