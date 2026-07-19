4e0cd3170590d15e759bfb438a153dc34e0fa062
Rami-Ebro <ahr17h@gmail.com>
2026-07-19 17:43:16 +0300

    Refine how we help service areas

---
+++ src/components/sections/how-we-help-areas.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+import { Eye,MonitorSmartphone,Gem,TrendingUp,Settings2,HandHeart } from "lucide-react";
+import { Section,SectionHeading } from "@/components/site/section-primitives";
+const icons=[Eye,MonitorSmartphone,Gem,TrendingUp,Settings2,HandHeart];
+const examplesAr=["تشخيص الوضع، ترتيب الأولويات، وتحديد القرار التالي","مواقع، متاجر إلكترونية، منصات، أنظمة داخلية، وأتمتة","التموضع، الهوية البصرية، الرسائل، وتجربة العلام
